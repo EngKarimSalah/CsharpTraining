0a151e97eb3079c34292f6fdfcb25319042167d7
karim Salaheldeen <eng.karimsalaheldeen@gmail.com>
2026-05-12 09:38:47 +0400

    added project structure

--- Program.cs
+++ Program.cs
@@ -4,6 +4,38 @@
     {
         static void Main(string[] args)
         {
+            //system setup ( system storage )
+            /*
+                declare variables
+                add initial vaues
+            */
+
+
+
+
+            //system options
+            /*
+               display main menu ( set of console writelines to display options )
+               switch( option )
+            {
+               case 1 - 7
+            
+               inside each case ex: case 1: 
+
+                Case 1:
+                             display welcome screen
+                             display options for 1.atm services ( view balance )
+                             switch( atmOptions )
+                             {
+
+                             }
+
+                  break;
+            
+            }
+               
+
+            */
 
 
         }
