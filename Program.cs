914d9faf1e2b89e6e38cf480288f648f6dfabf01
karim Salaheldeen <eng.karimsalaheldeen@gmail.com>
2026-05-12 09:21:08 +0400

    implemented task 2 - print welcome message

--- Program.cs
+++ Program.cs
@@ -6,6 +6,11 @@
         {
             //Task 1
             Console.WriteLine("karim");
+
+            //Task 2
+            Console.WriteLine("enter your name");
+            string name = Console.ReadLine();
+            Console.WriteLine("Welcome " + name);
         }
     }
 }
