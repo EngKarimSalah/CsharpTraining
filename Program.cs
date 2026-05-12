ba8c7057cd7d0739736b0f4772b4397f6880bb91
karim Salaheldeen <eng.karimsalaheldeen@gmail.com>
2026-05-12 09:29:58 +0400

    cleared the project

--- Program.cs
+++ Program.cs
@@ -4,13 +4,8 @@
     {
         static void Main(string[] args)
         {
-            //Task 1
-            Console.WriteLine("karim");
 
-            //Task 2
-            Console.WriteLine("enter your name");
-            string name = Console.ReadLine();
-            Console.WriteLine("Welcome " + name);
+
         }
     }
 }
